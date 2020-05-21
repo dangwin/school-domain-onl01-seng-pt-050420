9a5e33e9d238f35a5442be572e068d62d93f6ca3
Dang Nguyen <danghwin@gmail.com>
2020-05-21 00:07:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,25 @@
 # code here!
 class School 
   
-  def initialize(name)
-    @name = name
+  def initialize(schools_name)
+    @schools_name =schools_name
     @roster = {}
   end 
   
   def roster 
     @roster 
     
+  
   end 
   
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include(grade) == false 
+      @roster[grade] = []
+    end
+    @roster[grade] << name 
+      
+      
   
 end 
